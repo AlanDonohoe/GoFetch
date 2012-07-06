4d6b49026bea059159165dbb2d54b31ae92a36bd
Alan Donohoe <alandonohoe@hotmail.com>
2012-07-06 15:31:28 +0100

    Updated index.html and GoFetchServlet

--- GoFetch/src/com/gofetch/GoFetchRequestBean.java
+++ GoFetch/src/com/gofetch/GoFetchRequestBean.java
@@ -1,5 +1,36 @@
 package com.gofetch;
 
 public class GoFetchRequestBean {
+	protected String url;
+	protected boolean facebookData;
+	protected boolean twitterData;
+	protected boolean backLinkData;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public boolean isFacebookData() {
+		return facebookData;
+	}
+	public void setFacebookData(boolean facebookData) {
+		this.facebookData = facebookData;
+	}
+	public boolean isTwitterData() {
+		return twitterData;
+	}
+	public void setTwitterData(boolean twitterData) {
+		this.twitterData = twitterData;
+	}
+	public boolean isBackLinkData() {
+		return backLinkData;
+	}
+	public void setBackLinkData(boolean backLinkData) {
+		this.backLinkData = backLinkData;
+	}
+	
+	
 	
 }
--- GoFetch/src/com/gofetch/GoFetchServlet.java
+++ GoFetch/src/com/gofetch/GoFetchServlet.java
@@ -1,11 +1,64 @@
 package com.gofetch;
 import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class GoFetchServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		
+		//////////
+		// get data from page and fill bean....
+		GoFetchRequestBean goFetchBean = new GoFetchRequestBean();
+		
+		goFetchBean.setUrl(req.getParameter("target_url"));
+		
+		String backlink_data = req.getParameter("backlink_data");
+		String facebook_data = req.getParameter("facebook_data");
+		String twitter_data = req.getParameter("twitter_data");
+		
+		if("ON".equals(backlink_data)){
+			goFetchBean.setBackLinkData(true);
+		} else{
+			goFetchBean.setBackLinkData(false);
+		} 
+		
+		if("ON".equals(facebook_data)){
+			goFetchBean.setFacebookData(true);
+		} else{
+			goFetchBean.setFacebookData(false);
+		} 
+		
+		if("ON".equals(twitter_data)){
+			goFetchBean.setTwitterData(true);
+		} else{
+			goFetchBean.setTwitterData(false);
+		} 
+		//
+		//////////////
+		
+		//////////////
+		// save the bean to the session object
+			//req.getSession().setAttribute("dataBean", goFetchBean);
+		//
+		//////////////
+		
+		try {
+			getServletContext().getRequestDispatcher("/index.html").forward(req,resp);
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
 	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		doGet(req, resp); // just direct to doGet(...)
+	}
+	
 }
