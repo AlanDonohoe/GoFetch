8b1e61cbf38001bc1eb3e4235d23beed113630bc
Alan Donohoe <alandonohoe@hotmail.com>
2012-09-27 22:58:23 +0100

    pushed all files.

---
+++ GoFetch/test/com/gofetch/controllers/GoFetchServletTest.java
@@ -0,0 +1,19 @@
+package com.gofetch.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GoFetchServletTest extends GoFetchServlet {
+
+	@Test
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
